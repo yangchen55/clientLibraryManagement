535a340213fe0560604639755c6b59de989dd076
yangchen55 <tseyang986@gmail.com>
2023-01-06 17:22:38 +1100

    changed teacher dashboard

--- src/component/pages/DashboardTeacher.js
+++ src/component/pages/DashboardTeacher.js
@@ -26,6 +26,23 @@ const DashboardTeacher = ({ children }) => {
         <hr></hr>
         <div></div>
       </Container>
+
+      <Container className=" mt-5 dash-page">
+        <h1>welcome to library management System</h1>
+        <hr></hr>
+        <div>
+          Leganto support for academics Quick start guide: tools and features
+          Leganto - Getting started tutorial: interactive guide while you use
+          Leganto Leganto guide: guidance on creating a course material list,
+          adding resources, sharing with your class and troubleshooting Leganto
+          introductory video
+        </div>
+      </Container>
+      <Container className=" mt-5 dash-page">
+        <h1>welcome to library management System</h1>
+        <hr></hr>
+        <div></div>
+      </Container>
     </LayoutTeacher>
   );
 };
